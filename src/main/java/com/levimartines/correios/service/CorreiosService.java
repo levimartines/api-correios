c7db16f82ae9208d1c0b6f7c83417a1976918066
levimartines <levi.mferreira@gmail.com>
2020-05-13 21:04:43 -0300

    melhoria código

--- src/main/java/com/levimartines/correios/service/CorreiosService.java
+++ src/main/java/com/levimartines/correios/service/CorreiosService.java
@@ -3,17 +3,6 @@ package com.levimartines.correios.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.io.StringReader;
 
 @Service
 public class CorreiosService {
@@ -21,16 +10,18 @@ public class CorreiosService {
     public Double getValorFrete(String cepDestino) {
         String cepFormatado = cepDestino.replaceAll("-", "");
         String valorFreteCorreios = getValorFreteFromCorreios(cepFormatado);
-        return Double.parseDouble(valorFreteCorreios) + 4.0;
+        return Double.parseDouble(valorFreteCorreios);
     }
 
     private String getValorFreteFromCorreios(String cep) {
         RestTemplate restTemplate = new RestTemplate();
         final String baseUrl = "http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?" +
                 "sCepOrigem=18051020&" +
-                "nVlPeso=1&nCdFormato=1&" +
+                "nVlPeso=1&" +
+                "nCdFormato=1&" +
                 "nVlComprimento=16&" +
-                "nVlAltura=5&nVlLargura=11&" +
+                "nVlAltura=5&" +
+                "nVlLargura=11&" +
                 "sCdMaoPropria=n&" +
                 "nVlValorDeclarado=0&" +
                 "sCdAvisoRecebimento=n&" +

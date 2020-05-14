73bb05a6b5c56231c9a722ab3ba83bc31ced97a9
levimartines <levi.mferreira@gmail.com>
2020-05-13 21:25:29 -0300

    fix cors

--- src/main/java/com/levimartines/correios/controller/FreteController.java
+++ src/main/java/com/levimartines/correios/controller/FreteController.java
@@ -4,14 +4,12 @@ import com.levimartines.correios.service.CorreiosService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("api/v1")
+@CrossOrigin(origins = "https://levi-dev.myshopify.com/")
 public class FreteController {
 
     @Autowired
